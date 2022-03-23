f4dc9b59f7476e7c5dca2ff41881747706c7480b
Fridjinah <48133253+JinahC@users.noreply.github.com>
2022-03-23 17:25:28 -0400

    add new form + function

--- index.php
+++ index.php
@@ -7,9 +7,28 @@
   </head>
   <body>
    <h2>Current Sales</h2>
+   <form action="index.php" method="POST">
+     <label for="fname">First Name</label><br>
+    <input type="text" name="first_name" id="fname"><br><br>
+
+    <label for="lname">Last Name</label><br>
+    <input type="text" name="last_name" id="lname"><br><br>
+
+    <label for="id">Customer ID</label><br>
+    <input type="number" name="customer_id" id="id"><br><br>
+
+    <label for="city">City</label><br>
+    <input type="text" name="city" id="city"><br><br>
+
+    <label for="state">State</label><br>
+    <input type="text" name="state" id="state"><br><br>
+
+    <input type="submit" name="submit" value="Submit">
+   </form>
     <?php
        connect();
        salesTable();
+       updateTable();
        close();
     ?>
   </body>
